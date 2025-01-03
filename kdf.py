acb049d2aadfd2739d4e4060f7a0f55804f9026b
Cyberoiide <clement.bosle.dev^gmail.com>
2025-01-03 16:08:22 +0000

    sha 256 added

--- kdf.py
+++ kdf.py
@@ -1,9 +1,9 @@
-import hashlib
+from hash import generate_hash
 
 def derive_key(password):
     key = password.encode()
     for _ in range(10000):
-        key = hashlib.sha256(key).digest()
+        key = generate_hash(key)
     derived_key = key.hex()
     print(f"[DEBUG] Clé dérivée pour le mot de passe '{password}': {derived_key}")
     return derived_key
